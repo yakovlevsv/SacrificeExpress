e79951304fea21601d1d40354461a77fd965ea3f
Oplesnin Alexander <gasparfx@gmail.com>
2018-12-01 17:12:48 +0300

    Character movement

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    [SerializeField]
+    float _speed;
+
+    Rigidbody _rigidbody;
+
+    void Awake()
+    {
+        _rigidbody = GetComponentInChildren<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        var direction = new Vector3(h, 0, v);
+        if (direction != Vector3.zero)
+        {
+            direction.Normalize();
+            _rigidbody.velocity = direction * _speed;
+        }
+    }
+}
