025abcfa08e3c61a8eaddf3eb87808ab930862a3
kamergen <2kamergen@gmail.com>
2018-12-02 05:38:04 +0300

    add snow

---
+++ Assets/Scripts/Snow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Snow : MonoBehaviour
+{
+    public GameObject snow;
+
+    private void Awake()
+    {
+        snow.SetActive(false);
+    }
+
+    public void StartSnow()
+    {
+        snow.SetActive(true);
+        Animation animation = snow.GetComponent<Animation>();
+
+        animation.Play("Snow");
+    }
+
+    public void EndSnow()
+    {
+        snow.SetActive(false);
+    }
+}
